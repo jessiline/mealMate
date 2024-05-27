9e1055c5b967e53c5dac4744f129c8cea6b629bf
jessiline <jessiline.imanuela@gmail.com>
2024-05-27 14:14:36 +0700

    Initial Commit

---
+++ mealMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mealMate
+//
+//  Created by jessiline imanuela on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mealMate/mealMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealMateApp.swift
+//  mealMate
+//
+//  Created by jessiline imanuela on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct mealMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
